c65050f0dbecc3c5928eb40b2e020f9f0d048347
Jason Brown <jasontbrown99@gmail.com>
2017-02-17 11:30:27 -0800

    get and post to ui pages

--- app/app.php
+++ app/app.php
@@ -16,9 +16,13 @@
 
 
     $app->get("/", function() use ($app){
-        return "hello world";
+        return $app['twig']->render('form.html.twig');
     });
 
-
+    $app->post("/view_results", function() use ($app) {
+        $create_search = new RepeatCounter;
+        $get_occurences = $create_search->CountRepeats($_POST['sentence'], $_POST['search_word']);
+        return $app['twig']->render('result.html.twig', array('result' => $get_occurences));
+    });
 
     return $app;
