a1275f90d81c19d0e0ff6f29c95667103045d66e
jb <jasontbrown99@gmail.com>
2017-02-20 16:19:00 -0800

    change counter in foreach loop to ++counter and change variable in comparison operator to

--- src/WordFrequency.php
+++ src/WordFrequency.php
@@ -24,9 +24,7 @@
             $_SESSION['input'] = array();
         }
 
-        // Tyler, I feel like this should work.  It clearly doesn't, or else you wouldn't be reading this.  I feel like I might be missing something in between the foreach statement and the if conditional statement, but am not sure why or what that would be?  I feel like my if statement will do what I want it to do while iterating through the array.
 
-        //I've changed this around from my the code I submitted because, while the substr_count was awesome and returned a numerical value with ease, it was accounting for partial words and in my nervousness on Friday, I forgot to test for that.  I've been struggling with loops, both for and foreach.  When my partner does them, it makes total sense, but when I'm on my own it get a little lost if its something outside a course example.  I'm a little concerned going into databases as I feel that looping and arrays are going to be ESSENTIAL in data manipulation.  
         function CountRepeats($string_input, $search_for_word)
         {
             // counter set to zero
@@ -39,9 +37,9 @@
             // For each seperated word within string input array,
             foreach ($string_input_array as $key=>$value) {
                 // if the value matches search_for_word
-                if ($value == $this->search_for_word) {
+                if ($value == $search_for_word) {
                     // add 1 for every match
-                    $counter +=1;
+                    ++$counter;
                 }
             }
             var_dump($counter);
